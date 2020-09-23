b627e7bbce9b34517f5cc906fdca4a59f432b573
John Hoffer <john@hoff.in>
2020-09-23 09:22:04 -0400

    only build_page in index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import {get_links_alias} from './links_alias.js'
 import {build_page} from './main.js'
 
 export default {
-  build_page: build_page,
-  get_links_alias: get_links_alias
+  build_page: build_page
 }

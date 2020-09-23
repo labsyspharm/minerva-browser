75f4a014deae4ce09afc7b471e0a3d7e49469832
John Hoffer <john@hoff.in>
2020-09-23 09:53:38 -0400

    add openseadragon and openseadragon plugins

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import {build_page} from './main.js'
+import {build_page} from './main.js';
+import OpenSeadragon from 'openseadragon';
+import {OpenSeadragonScalebar} from './openseadragon-scalebar.js';
+import {OpenSeadragonSvgOverlay} from './openseadragon-svg-overlay.js';
+OpenSeadragonScalebar(OpenSeadragon);
+OpenSeadragonSvgOverlay(OpenSeadragon);
 
 export default {
+  OpenSeadragon: OpenSeadragon,
   build_page: build_page
 }

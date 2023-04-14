15661531d810cb7d7db0a9466b2b2d1125c68e3f
John Hoffer <john@hoff.in>
2023-04-14 13:03:40 -0400

    multichannel rendering, still working on lensing

--- channel.js
+++ channel.js
@@ -107,44 +107,61 @@ const draw_tile = (props, output, via) => {
 
 const render_tile = (props, uniforms, via) => {
   const { gl } = via;
-  const { 
-    frame, name, data, is_rendered_1i, color_3fv
-  } = props;
+  const { frame, data, colors } = props;
   const {
-    u_is_rendered, u_tile_shape, u_tile_color
+    u_shape, u_t0_color, u_t1_color,
+    u_t2_color, u_t3_color, u_t4_color,
+    u_t5_color, u_t6_color, u_t7_color
   } = uniforms;
   const w = data.width;
   const h = data.height;
   const tile_shape_2fv = new Float32Array([w, h]);
-  gl.uniform1i(u_is_rendered, +is_rendered_1i);
-  gl.uniform2fv(u_tile_shape, tile_shape_2fv);
-  gl.uniform3fv(u_tile_color, color_3fv);
+  const black = hex2gl("000000");
+  gl.uniform2fv(u_shape, tile_shape_2fv);
+  gl.uniform3fv(u_t0_color, colors[0] || black);
+  gl.uniform3fv(u_t1_color, colors[1] || black);
+  gl.uniform3fv(u_t2_color, colors[2] || black);
+  gl.uniform3fv(u_t3_color, colors[3] || black);
+  gl.uniform3fv(u_t4_color, colors[4] || black);
+  gl.uniform3fv(u_t5_color, colors[5] || black);
+  gl.uniform3fv(u_t6_color, colors[6] || black);
+  gl.uniform3fv(u_t7_color, colors[7] || black);
 
-  // Send the tile to the texture
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB8UI, w, h, 0,
-            gl.RGB_INTEGER, gl.UNSIGNED_BYTE, data);
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  // Send the tile channels to the texture
+  [0,1,2,3,4,5,6,7].forEach((i) => {
+    const from = data.channels[i];
+    if (from === undefined) return;
+    gl.activeTexture(gl['TEXTURE'+i]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB8UI, w, h, 0,
+              gl.RGB_INTEGER, gl.UNSIGNED_BYTE, from.data);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  });
   draw_tile(props, gl.canvas, via);
 }
 
-const to_tile_props = (closure, sources, d, e) => {
+const to_tile_props = (closure, shown, d, e) => {
   const { state } = closure;
-  const is_rendered_1i = false;
-  const { name, data } = sources[0]; // TODO
-  const color_3fv = state.channel_map.get(name).color;
+  const data = {
+    channels: shown,
+    width: shown[0].data.width,
+    height: shown[0].data.height
+  }
+  const colors = shown.map(show => {
+    return state.channel_map.get(show.name).color;
+  });
   const context = d._getContext(false)
   const rendered = e.rendered;
   const frame = to_tile_frame(e);
 
   return {
-    rendered, context, frame, name, data, is_rendered_1i, color_3fv 
+    rendered, context, frame, data, colors 
   }
 }
 
-const to_render_target = (closure, sources) => {
+const to_render_target = (closure, shown) => {
   const { via, state, uniforms } = closure;
   return (d, e) => {
-    const props = to_tile_props(closure, sources, d, e);
+    const props = to_tile_props(closure, shown, d, e);
     if (props) render_tile(props, uniforms, via);
   }
 }
@@ -156,19 +173,19 @@ const copy_sources = (closure, e) => {
   const source_count = state.active_subgroups.length;
   const all_sources = sources.size === source_count;
   const d = viewer.drawer;
-  if (!sources.size) return null;
+  if (!sources.size) return;
   // Redraw the target with correct info
-  // TODO: use mutiple channels
-  const favored = state.active_subgroups[0].Name
-  const visible = [...sources].filter(source => {
-    return state.channel_map.has(source.name);
-  });
-  const source = visible.find((source, i) => {
-    if (source.name === favored) return true;
-    return i = visible.size - 1; // catch-all
-  });
-  if (source === undefined) return;
-  to_render_target(closure, [source])(d, e);
+  const source_map = [...sources].reduce((o, source) => {
+    o.set(source.name, source);
+    return o;
+  }, new Map);
+  const shown = state.active_subgroups.filter((sub) => {
+    if (!state.channel_map.has(sub.Name)) return false;
+    if (!source_map.has(sub.Name)) return false;
+    return true;
+  }).map(sub => source_map.get(sub.Name));
+  if (!shown.length) return;
+  to_render_target(closure, shown)(d, e);
 }
 
 const to_tile_drawing = (closure) => {
@@ -181,6 +198,9 @@ const to_tile_drawing = (closure) => {
     const missing = ![ is_source, is_target ].some(x => x);
     // Unable to colorize this layer
     if (missing) return; 
+    if (e.tile._cached) return;
+
+    console.log('HI');
 
     // Clear any rendered tile
     const w = e.rendered.canvas.width;
@@ -189,7 +209,10 @@ const to_tile_drawing = (closure) => {
     e.rendered.fillRect(0, 0, w, h);
 
     // Copy all sources to target
-    if (is_target) copy_sources(closure, e);
+    if (is_target) {
+      copy_sources(closure, e);
+      e.tile._cached = true;
+    }
   }
 }
 
@@ -199,42 +222,52 @@ precision highp int;
 precision highp float;
 precision highp usampler2D;
 
-uniform int u_is_rendered;
-uniform vec2 u_tile_shape;
-uniform vec3 u_tile_color;
-uniform usampler2D u_tile;
+uniform vec2 u_shape;
+uniform vec3 u_t0_color;
+uniform vec3 u_t1_color;
+uniform vec3 u_t2_color;
+uniform vec3 u_t3_color;
+uniform vec3 u_t4_color;
+uniform vec3 u_t5_color;
+uniform vec3 u_t6_color;
+uniform vec3 u_t7_color;
+uniform usampler2D u_t0;
+uniform usampler2D u_t1;
+uniform usampler2D u_t2;
+uniform usampler2D u_t3;
+uniform usampler2D u_t4;
+uniform usampler2D u_t5;
+uniform usampler2D u_t6;
+uniform usampler2D u_t7;
 
 in vec2 uv;
 out vec4 color;
 
 // Sample texture at given texel offset
-uvec4 offset(usampler2D sam, vec2 size, vec2 pos, vec2 off) {
+uvec4 texel(usampler2D sam, vec2 size, vec2 pos, vec2 off) {
   float x = pos.x + off.x / size.x;
   float y = pos.y + off.y / size.y;
   return texture(sam, vec2(x, y));
 }
 
 // Colorize continuous u8 signal
-vec4 u8_r_range(float alpha) {
-  uint pixel = offset(u_tile, u_tile_shape, uv, vec2(0, 0)).r;
-  float value = float(pixel) / 255.;
-
-  // Color pixel value
-  vec3 pixel_color = u_tile_color * value;
-  return vec4(pixel_color, alpha);
+vec3 u8_r_range(usampler2D sam, vec3 color) {
+  vec2 xy = vec2(0, 0);
+  // Scale color by texel value 
+  uint scale = texel(sam, u_shape, uv, xy).r;
+  return color * float(scale) / 255.;
 }
 
 void main() {
-  if (u_is_rendered == 1) {
-    uvec4 rgb = offset(u_tile, u_tile_shape, uv, vec2(0, 0));
-    float r = float(rgb.r)/255.;
-    float g = float(rgb.g)/255.;
-    float b = float(rgb.b)/255.;
-    color = vec4(r, g, b, 1.0);
-  }
-  else {
-    color = u8_r_range(1.0);
-  }
+  vec3 v0 = u8_r_range(u_t0, u_t0_color);
+  vec3 v1 = u8_r_range(u_t1, u_t1_color);
+  vec3 v2 = u8_r_range(u_t2, u_t2_color);
+  vec3 v3 = u8_r_range(u_t3, u_t3_color);
+  vec3 v4 = u8_r_range(u_t4, u_t4_color);
+  vec3 v5 = u8_r_range(u_t5, u_t5_color);
+  vec3 v6 = u8_r_range(u_t6, u_t6_color);
+  vec3 v7 = u8_r_range(u_t7, u_t7_color);
+  color = vec4(v0+v1+v2+v3+v4+v5+v6+v7, 1.0);
 }`,
   VERTEX_SHADER: `#version 300 es
 in vec2 a_uv;
@@ -286,13 +319,27 @@ const toBuffers = (program, via) => {
   const gl = via.gl;
 
   // Get GLSL locations
-  const u_tile = gl.getUniformLocation(program, 'u_tile');
+  const u_t0 = gl.getUniformLocation(program, 'u_t0');
+  const u_t1 = gl.getUniformLocation(program, 'u_t1');
+  const u_t2 = gl.getUniformLocation(program, 'u_t2');
+  const u_t3 = gl.getUniformLocation(program, 'u_t3');
+  const u_t4 = gl.getUniformLocation(program, 'u_t4');
+  const u_t5 = gl.getUniformLocation(program, 'u_t5');
+  const u_t6 = gl.getUniformLocation(program, 'u_t6');
+  const u_t7 = gl.getUniformLocation(program, 'u_t7');
   const a_uv = gl.getAttribLocation(program, 'a_uv');
   const u8 = gl.getUniformLocation(program, 'u8');
 
   // Assign uniform values
   gl.uniform1ui(u8, 255);
-  gl.uniform1i(u_tile, 0);
+  gl.uniform1i(u_t0, 0);
+  gl.uniform1i(u_t1, 1);
+  gl.uniform1i(u_t2, 2);
+  gl.uniform1i(u_t3, 3);
+  gl.uniform1i(u_t4, 4);
+  gl.uniform1i(u_t5, 5);
+  gl.uniform1i(u_t6, 6);
+  gl.uniform1i(u_t7, 7);
 
   // Assign vertex inputs
   gl.bindBuffer(gl.ARRAY_BUFFER, via.buffer);
@@ -301,19 +348,21 @@ const toBuffers = (program, via) => {
   // Enable vertex buffer
   gl.enableVertexAttribArray(a_uv);
   gl.vertexAttribPointer(a_uv, 2, gl.FLOAT, 0, via.one_point_size,
-                         0 * via.points_list_size)
+                         0 * via.points_list_size);
 
-  // Set Texture for GLSL
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, via.texture),
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+  [0,1,2,3,4,5,6,7].forEach((i) => {
+    // Set Texture
+    gl.activeTexture(gl['TEXTURE'+i]);
+    gl.bindTexture(gl.TEXTURE_2D, via.textures[i]);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
 
-  // Assign texture parameters
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // Assign texture parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  })
 }
 
 const update_shape = (gl, { width, height }) => {
@@ -329,9 +378,8 @@ const initialize_gl = (tileShape) => {
   update_shape(gl, tileShape);
   gl.useProgram(program);
   toBuffers(program, {
-    gl, texture: gl.createTexture(),
-    buffer: gl.createBuffer(),
-    ...to_vertices()
+    gl, ...to_vertices(), buffer: gl.createBuffer(),
+    textures: [0,1,2,3,4,5,6,7].map(() => gl.createTexture())
   });
   const via = { ...tileShape, gl };
   return { program, via };
@@ -368,10 +416,20 @@ const linkShaders = (props) => {
 
   // Initialize WebGL
   const { program, via } = initialize_gl(toTileShape(tileSources))
-  const u_tile_shape = via.gl.getUniformLocation(program, "u_tile_shape");
-  const u_is_rendered = via.gl.getUniformLocation(program, "u_is_rendered");
-  const u_tile_color = via.gl.getUniformLocation(program, "u_tile_color");
-  const uniforms = { u_is_rendered, u_tile_shape, u_tile_color };
+  const u_shape = via.gl.getUniformLocation(program, "u_shape");
+  const u_t0_color = via.gl.getUniformLocation(program, "u_t0_color");
+  const u_t1_color = via.gl.getUniformLocation(program, "u_t1_color");
+  const u_t2_color = via.gl.getUniformLocation(program, "u_t2_color");
+  const u_t3_color = via.gl.getUniformLocation(program, "u_t3_color");
+  const u_t4_color = via.gl.getUniformLocation(program, "u_t4_color");
+  const u_t5_color = via.gl.getUniformLocation(program, "u_t5_color");
+  const u_t6_color = via.gl.getUniformLocation(program, "u_t6_color");
+  const u_t7_color = via.gl.getUniformLocation(program, "u_t7_color");
+  const uniforms = {
+    u_shape, u_t0_color, u_t1_color,
+    u_t2_color, u_t3_color, u_t4_color,
+    u_t5_color, u_t6_color, u_t7_color
+  };
   viewer.addHandler('tile-drawing', to_tile_drawing({
     via, viewer, state, uniforms
   }));

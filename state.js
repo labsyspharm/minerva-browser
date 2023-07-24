da1f732e6e81d021c3f8e877c116076cb2913e22
John Hoffer <john@hoff.in>
2023-07-24 12:05:45 -0400

    v3.3.6 postMessage

--- state.js
+++ state.js
@@ -916,6 +916,8 @@ HashState.prototype = {
       else {
         history.pushState(this.design, document.title, url);
       }
+      const href = window.location.origin + url;
+      parent.postMessage({ href }, "*");
 
       this.changed = false;
     }
